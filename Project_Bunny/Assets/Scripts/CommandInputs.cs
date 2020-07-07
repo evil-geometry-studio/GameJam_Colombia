f694bcbc212c53c6fc3e7ea5918ce73780138c8f
DeveloperVix <vicomisterio@hotmail.com>
2020-07-06 22:57:16 -0500

    Patron de diseño Command
    Se ha implementado el patron de diseño command para los inputs por parte del jugador, esto permitirá intercambiar los botóne para las diferentes acciones que se pueden hacer, que son 4.

---
+++ Project_Bunny/Assets/Scripts/CommandInputs.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/*
+    Patron de diseño comando, esto permite intercambiar los inputs, siempre mandando llamar
+    a la misma acción
+
+*/
+
+namespace CommandInput
+{
+    public static class CommandInputs
+    {
+        //Las acciones que se pueden ejecutar
+        public enum TheKeysButtons { GoRight, GoLeft, CreateGap, InteractionObjs }
+        //En que estado se encuentran los inputs, puede ser que ya se hayan invertido
+        //Si se quieren más estados, añadirlo al enum y crear la sentencia if correspondiente en el metodo GetKeyCode
+        public enum StateInput { Default, Inverted_1, }
+
+        //El estado cuando un boton es presionado a fondo
+        public static bool GetKeyButtonDown(TheKeysButtons btn, StateInput state)
+        {
+            KeyCode code = GetKeyCode(btn, state);
+            return Input.GetKeyDown(code);
+        }
+
+        //Se libera el boton
+        public static bool GetKeyButtonUp(TheKeysButtons btn, StateInput state)
+        {
+            KeyCode code = GetKeyCode(btn, state);
+            return Input.GetKeyUp(code);
+        }
+
+        //Si se esta presionando una tecla
+        public static bool GetKeyButton(TheKeysButtons btn, StateInput state)
+        {
+            KeyCode code = GetKeyCode(btn, state);
+            return Input.GetKey(code);
+        }
+
+        static KeyCode GetKeyCode(TheKeysButtons btn, StateInput state)
+        {
+            //Se pregunta por el estado actual del boton
+            if (state == StateInput.Default)
+            {
+                //Estado normal, devuelve el la asignación de la acción al estado correspondiente
+                switch (btn)
+                {
+                    case TheKeysButtons.GoRight: return KeyCode.D;
+                    case TheKeysButtons.GoLeft: return KeyCode.A;
+                    case TheKeysButtons.CreateGap: return KeyCode.Mouse0;
+                    case TheKeysButtons.InteractionObjs: return KeyCode.Mouse1;
+                }
+            }
+            else if(state == StateInput.Inverted_1)
+            {
+                //Estado invertido 1, devuelve el la asignación de la acción al estado correspondiente
+                switch (btn)
+                {
+                    case TheKeysButtons.GoRight: return KeyCode.Mouse1;
+                    case TheKeysButtons.GoLeft: return KeyCode.Mouse0;
+                    case TheKeysButtons.CreateGap: return KeyCode.A;
+                    case TheKeysButtons.InteractionObjs: return KeyCode.D;
+                }
+            }
+
+            return KeyCode.None;
+        }
+    }
+}
+
+
--- Project_Bunny/Assets/Scripts/PlayerMovement.cs
+++ Project_Bunny/Assets/Scripts/PlayerMovement.cs
@@ -1,81 +1,118 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CommandInput;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Velocidad de movimiento del personaje
     public float speedMove = 5f;
 
+    //Referencia al cuerpo rigido del personaje
     Rigidbody2D rbd2D;
 
-    int direction = 0;
+    //Direccion de movimiento en base a las teclas presionadas
+    int directionMove = 0;
 
+    //Referencia a la camara de juego, hay que arrastrala desde el inspector
     public Camera mainCamera;
 
-    public float countTime = 0f;
+    //La posicion a donde se dirigirá el jugador una vez que se haya creado la brecha
+    [HideInInspector]
+    public Vector3 gapPosition;
 
+    //Fuerza que se aplicará para desplazarse a la brecha
+    public float forceOnTime = 0f;
+
+    //Bandera para indicar cuando se puede incrementar la fuerza que se aplicará
     bool hitIncrement;
 
-    public float fuerzaBrecha = 3;
+    //Fuerza que se suma cada tiempo que se mantiene presionado el mouse
+    public float forceAdded = 3;
 
-    public float lastCountTime;
+    //La fuerza que se le aplicará al jugador para moverse hacia la brecha
+    public float forceAppliedToMove;
+
+    [Header("State of the Inputs")]
+    public CommandInputs.StateInput curState;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Se obtiene el componente RigidBody2D
         rbd2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        if (CommandInputs.GetKeyButtonDown(CommandInputs.TheKeysButtons.GoRight, curState))
         {
-            direction = 1;
+            directionMove = 1;
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (CommandInputs.GetKeyButtonDown(CommandInputs.TheKeysButtons.GoLeft, curState))
         {
-            direction = -1;
+            directionMove = -1;
         }
 
-        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        if (CommandInputs.GetKeyButtonUp(CommandInputs.TheKeysButtons.GoRight, curState) || 
+            CommandInputs.GetKeyButtonUp(CommandInputs.TheKeysButtons.GoLeft, curState))
         {
-            direction = 0;
+            directionMove = 0;
         }
 
-        ///Deteccion de mouse
+        //Interacción con objetos
 
-        if (Input.GetMouseButtonDown(0))
+        if(CommandInputs.GetKeyButtonDown(CommandInputs.TheKeysButtons.InteractionObjs, curState))
         {
-            Debug.Log("Mouse izquierdo");
-
+            //Esto para dar click en objetos que tengan un collider en 2D,
             RaycastHit2D hit = Physics2D.GetRayIntersection(mainCamera.ScreenPointToRay(Input.mousePosition));
             if (hit.collider != null)
             {
-                Debug.Log(hit.transform.position);
-                //Habilitar el conteo de tiempo para aplicar fuerza,
+                Debug.Log("Interactuo con el objeto: "+hit.collider.name);
             }
         }
-        if (Input.GetMouseButton(0) && hitIncrement == true)
-        {
-            countTime += Time.deltaTime * fuerzaBrecha;
 
-            if (countTime > 10f)
+        ///Creación de brecha
+
+        if (CommandInputs.GetKeyButtonDown(CommandInputs.TheKeysButtons.CreateGap, curState))
+        {
+            //Debug.Log(mainCamera.ScreenPointToRay(Input.mousePosition));
+            gapPosition = mainCamera.ScreenPointToRay(Input.mousePosition).origin;
+            hitIncrement = true;  
+        }
+        if (CommandInputs.GetKeyButton(CommandInputs.TheKeysButtons.CreateGap, curState) && hitIncrement == true)
+        {
+            forceOnTime += Time.deltaTime * forceAdded;
+
+            if (forceOnTime > 10f)
             {
-                countTime = 10f;
+                forceOnTime = 10f;
+                //Temporal, ya que se tiene que añadir el como se hace la transición, esto lo teletrasnporta de forma inmediata
+                transform.position = gapPosition;
+                
+                hitIncrement = false;
             }
         }
-        else if (Input.GetMouseButtonUp(0))
+        else if (CommandInputs.GetKeyButtonUp(CommandInputs.TheKeysButtons.CreateGap, curState))
         {
-            lastCountTime = countTime;
-            countTime = 0f;
+            forceAppliedToMove = forceOnTime;
             hitIncrement = false;
+            
+            if(forceOnTime < 10)
+            {
+                //Temporal, ya que se tiene que añadir el como se hace la transición, esto lo teletrasnporta de forma inmediata
+                transform.position = gapPosition;
+            }
+            
+            forceOnTime = 0f;
         }
     }
 
     private void FixedUpdate()
     {
-        rbd2D.velocity = new Vector2(direction * speedMove, rbd2D.velocity.y);
+        //Aplica la velocidad de desplazamiento dependiendo de la dirección
+        rbd2D.velocity = new Vector2(directionMove * speedMove, rbd2D.velocity.y);
     }
 }
